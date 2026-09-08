d228952979b3d206f1bbc698055b4f70f77a27de
ashmit katiyar <ashmitkatiyar222@gmail.com>
2026-09-08 10:32:23 +0530

    final commit

--- lab3/app.js
+++ lab3/app.js
@@ -1,5 +1,6 @@
 import http from "http";
-import { getAllTeam } from "./teams.js";
+import { getAllTeam,addteam } from "./teams.js";
+import {parse as parseUrl} from "url";
 
 
 const sendJson=(res,statusCode,data)=>{
@@ -28,9 +29,28 @@ const parseJSONBody = (req) => {
     });
 };
 
-const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.end("<h2 style='color:black;'>welcome to server side</h2>")
+const server = http.createServer(async(req, res) => {
+    const {pathname,query}=parseUrl(req.url,true);
+    const {method}=req;
+    console.log('pathname',pathname);
+    console.log('query',query);
+    console.log('method',method);
+
+    if(pathname==='/api/v1/teams'&&method==='GET'){
+
+        
+        sendJson(res,200,getAllTeam());
+    }else if(pathname==='/api/v1/teams'&&method==='POST'){
+        await parseJSONBody(req).then((teamData)=>{
+            addteam(teamData);
+            sendJson(res,201,{message:"Team added successfully"});
+        }).catch((error)=>{
+            sendJson(res,400,{error:"Invalid JSON data"});
+        });
+    }else{
+        res.statusCode=404;
+        res.end();
+    }
 });
 
 const port = 5000;
