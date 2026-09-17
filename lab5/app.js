b5c7cbb8a7df840dfc097cb61282c3d2d79fb467
ashmit katiyar <ashmitkatiyar222@gmail.com>
2026-09-17 13:58:43 +0530

    5 api

--- lab5/app.js
+++ lab5/app.js
@@ -5,7 +5,28 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("<h1>hello</h1>");
 });
-app.use( (req,res)=>{
+
+
+app.get('/about', (req, res) => {
+    res.send("<h1>we are stupids</h1>");
+
+});
+app.post('/login',(req,res)=>{
+    res.send({msg:'user login'})
+
+});
+
+app.put('/users/update/1',(req,res)=>{
+res.send({msg:'user updated'});
+});
+
+app.delete('/users/1',(req,res)=>{
+    res.send({msg:'remove user 1'})
+})
+
+
+
+app.use((req, res) => {
     res.status(404).send("<h1>404 Not Found</h1>");
     res.end();
 });
---
+++ lab5/readme.md
@@ -0,0 +1,8 @@
+#express
+
+fast,unopinionated minimalist web frame work for node js
+create np project `npm init-y`
+update package.json
+"module"
+install nodemon with `npm i nodemon -D`
+install express with `npm i express`
